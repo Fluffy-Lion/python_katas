ff178c0996f3dbf0263361c0409d9f3cc4319efa
Fluffy-Lion <leoncooper31@gmail.com>
2021-03-16 17:44:41 +0000

    added sheep count

--- arrays.py
+++ arrays.py
@@ -1,8 +1,15 @@
 # get the product of all elements in an array
-def products(nums):
-    answer = 1
-    for num in nums:
-        answer = answer * num
-    return answer
+# def products(nums):
+#     answer = 1
+#     for num in nums:
+#         answer = answer * num
+#     return answer
 
-print(products([1, 2, 3, 4, 5, 6]))
+# print(products([1, 2, 3, 4, 5, 6]))
+
+# counting sheep
+# count the number of sheep present (true) in its place
+sheep = [ True, False, True, True, True, False, True, True,]
+
+count = sum(sheep)
+print(count)
