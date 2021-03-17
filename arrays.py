429b44b1fe1a89b310c7152715bd6367be575041
Fluffy-Lion <leoncooper31@gmail.com>
2021-03-17 22:26:27 +0000

    get planet name

--- arrays.py
+++ arrays.py
@@ -9,7 +9,25 @@
 
 # counting sheep
 # count the number of sheep present (true) in its place
-sheep = [ True, False, True, True, True, False, True, True,]
+# sheep = [ True, False, True, True, True, False, True, True,]
 
-count = sum(sheep)
-print(count)
+# count = sum(sheep)
+# print(count)
+
+# display the correct planet name
+# user types in a number as an argument
+# to return the correct planet
+# if 3 is an argument, should return EARTH
+def get_planet_name(id):
+    name= ""
+    switch_id = {
+         1: "Mercury",
+         2: "Venus",
+         3: "Earth",
+         4: "Mars",
+         5: "Jupiter",
+         6: "Saturn",
+         7: "Uranus", 
+         8: "Neptune",
+            }
+    return switch_id.get(id)
