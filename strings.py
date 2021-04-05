90db48ee1bd3886b5838621d2e559d4f3defdd9e
Fluffy-Lion <leoncooper31@gmail.com>
2021-04-05 13:18:40 +0100

    error fixed for status question

--- strings.py
+++ strings.py
@@ -44,3 +44,6 @@
 # Function should return a dictionary/Object/Hash with "status" as a key, 
 # whose value can : "busy" or "available" depending on the truth value of the argument is_busy.
 
+# def get_status(is_busy):
+#     status = "busy" if is_busy else "available"
+#     return {"status" : status}
