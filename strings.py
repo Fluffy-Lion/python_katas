e3a91bb592f6d042ff37992b646e3a19e369560f
Fluffy-Lion <leoncooper31@gmail.com>
2021-04-26 20:31:15 +0100

    format dollars

--- strings.py
+++ strings.py
@@ -47,3 +47,34 @@
 # def get_status(is_busy):
 #     status = "busy" if is_busy else "available"
 #     return {"status" : status}
+
+# >>>>>>>>>>>>>>
+
+# The company you work for has just been awarded a contract to build a payment gateway. 
+# In order to help move things along, you have volunteered to create a function that will take a 
+# float and return the amount formatting in dollars and cents.
+
+# 39.99 becomes $39.99
+
+# The rest of your team will make sure that the argument is sanitized before being passed to
+#  your function although you will need to account for adding trailing zeros if they are missing 
+#  (though you won't have to worry about a dangling period).
+
+# def format_money(amount):
+#     num = str(amount)
+#     rvrs = num[::-1]
+#     dot = rvrs.find(".")
+#     if dot == 1:
+#         rvrs = "0" + rvrs
+#         return "$" + rvrs[::-1]
+#     elif dot == 2:
+#         return "$" + rvrs[::-1]
+#     else:
+#         rvrs = "00." + rvrs
+#         return "$" + rvrs[::-1]
+# print(format_money(98))
+
+# or
+def format_money(amount):
+    return '${:.2f}'.format(amount)
+print(format_money(98))
