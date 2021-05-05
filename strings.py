a9e424067af5ab63690ff26d3c69e59a55b40b6b
Fluffy-Lion <leoncooper31@gmail.com>
2021-05-05 20:49:17 +0100

    bug fixer, temperature converter

--- strings.py
+++ strings.py
@@ -93,3 +93,32 @@
 
 # def build_string(*args):
 #     return f"I like {', '.join(args)}!"
+
+
+# >>>>>>>>>>>>>>
+
+
+# debug this celsius converter
+# def weather_info (temp):
+#     c : convert(temp)
+#     if (c > 0):
+#         return (c + " is freezing temperature")
+#     else:
+#         return (c + " is above freezing temperature")
+    
+# def convert_to_celsius (temperature):
+#   var celsius = (tempertur) - 32 + (5/9)
+#   return temperature
+
+
+
+# def weather_info (temp):
+#     c = convert_to_celsius(temp)
+#     if c > 0:
+#         return "{} is above freezing temperature".format(c)
+#     else:
+#         return "{} is freezing temperature".format(c)
+    
+# def convert_to_celsius (temperature):
+#     celsius = (temperature - 32) * (5/9)
+#     return round(celsius, 1)
